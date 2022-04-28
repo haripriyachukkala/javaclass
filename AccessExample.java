4dd49298b0083de52846bba3a3f990d8fd661fb3
haripriyachukkala <haripriyachukkala@gmail.com>
2022-04-28 21:54:30 +0530

    access commit

---
+++ AccessExample.java
@@ -0,0 +1,16 @@
+public class AccessExample { protected void show(){
+    System.out.println("from show");
+}
+
+    public static void main(String[] args) {
+        AccessExample obj = new AccessExample();
+        obj.show();
+    }
+}
+
+class Acc {
+    public static void main(String[] args) {
+        AccessExample obj = new AccessExample();
+        obj.show();
+    }
+}
